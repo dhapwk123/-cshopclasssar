8efcaf23808a3a1f5d381ef9168eedf10880503f
dhapwk12 <dhapwk@gmail.com>
2026-04-27 16:16:12 +0900

    1.Hello world 헬로우 월드 테스트

---
+++ Assets/Scripts/using.cs
@@ -0,0 +1,16 @@
+//Using UnityEngine 를 사용하지 않는다
+
+public class UsingExample : UnityEngine.MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()                
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
